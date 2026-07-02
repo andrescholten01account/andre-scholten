b6b64f55e4ff5e9065579daf70f6e3c7f5ccd2d6
andrescholten01account <andre.scholten@globalrize.nl>
2026-07-02 21:48:21 +0200

    vault backup: 2026-07-02 21:48:21

--- quartz/components/SiteHeader.tsx
+++ quartz/components/SiteHeader.tsx
@@ -26,8 +26,6 @@ const SiteHeader: QuartzComponent = ({ fileData, cfg }: QuartzComponentProps) =>
         <a href={`${baseDir}/de-volle-pinksterzegen---boek`}>Boek</a>
         <span class="nav-sep"> | </span>
         <a href={`${baseDir}/bijbel`}>Bijbel</a>
-        <span class="nav-sep"> | </span>
-        <a href={`${baseDir}/studiebijbel`}>StudieBijbel</a>
       </nav>
     </div>
   )
